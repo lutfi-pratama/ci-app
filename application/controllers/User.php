6c2f8afa1c3b72bbe827f400c51ebeace08707be
FransTan <fransiskus847@gmail.com>
2021-12-07 21:12:45 +0800

    Final

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -37,14 +37,29 @@ class User extends CI_Controller
             $name = $this->input->post('name');
             $email = $this->input->post('email');
 
-            // cek jika ada gambar yang akan diupload
-            
+            // // cek jika ada gambar yang akan diupload
+            // $upload_image = $_FILES['image']['name'];
+
+            // if($upload_image) {
+            //     $config['allowed_types'] = 'gif|jpg|png';
+            //     $config['max_size']     = '5256';
+            //     $config['upload_path'] = './assets/img/profile';
+
+            //     $this->load->library('upload', $config);
+
+            //     if($this->upload->do_upload('image')) {
+            //         $new_image = $this->upload->data('file_name');
+            //         $this->db->set('image', $new_image);
+            //     } else {
+            //         echo $this->upload->display_errors();
+            //     }
+            // }
 
             $this->db->set('name', $name);
-            $this->db->where('email');
+            $this->db->where('email', $email);
             $this->db->update('user');
 
-            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Your profile has been updated!</div>');
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Your profile has been updated!</div>');
             redirect('user');
         }
     }
